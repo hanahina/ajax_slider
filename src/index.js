cd3477d38ff27bd2d14fa8cf27c1ca15bc4d7fbf
Cynthia Hsu <works@hinahina.tw>
2019-10-17 16:29:08 +0800

    webpack 設定

--- src/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    const startTime = Date.now();
-    function log(item) {
-        const timeSpend = (Date.now() - startTime) / 1000;
-        console.log(`${item}, ${timeSpend}s`);
-    }
-    log("Hello!");
-})();
